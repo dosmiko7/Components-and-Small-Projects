1183b8c771a0088a29f2261eec2ebee27adf32f8
dosmiko7 <mikolaj.oberda@gmail.com>
2023-04-19 20:23:44 +0200

    useContext, useRef, useImperativeHandle, forwardRef examples

--- Simple-React-Apps/Learning-useEffect-Hook/src/components/Login/Login.js
+++ Simple-React-Apps/Learning-useEffect-Hook/src/components/Login/Login.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext, useRef } from "react";
+
+import classes from "./Login.module.css";
 
 import Card from "../UI/Card/Card";
-import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../context/auth-context";
+import Input from "../UI/Input/Input";
 
 const emailReducer = (state, action) => {
 	if (action.type === "USER_INPUT") {
@@ -34,6 +37,8 @@ const Login = (props) => {
 	// const [passwordIsValid, setPasswordIsValid] = useState();
 	const [formIsValid, setFormIsValid] = useState(false);
 
+	const authCtx = useContext(AuthContext);
+
 	const [emailState, dispatchedEmail] = useReducer(emailReducer, {
 		value: "",
 		isValid: false,
@@ -47,6 +52,9 @@ const Login = (props) => {
 	// Reducing useEffect executions count
 	const { isValid: emailIsValid } = emailState;
 	const { isValid: passwordIsValid } = passwordState;
+
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
 
 	// USE EFFECT HOOK
 	useEffect(() => {
@@ -89,37 +97,42 @@ const Login = (props) => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		props.onLogin(emailState.value, passwordState.value);
+		if (formIsValid) {
+			authCtx.onLogin(emailState.value, passwordState.value);
+		} else if (!emailIsValid) {
+			emailInputRef.current.focus();
+		} else {
+			passwordInputRef.current.focus();
+		}
 	};
 
 	return (
 		<Card className={classes.login}>
 			<form onSubmit={submitHandler}>
-				<div className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ""}`}>
-					<label htmlFor="email">E-Mail</label>
-					<input
-						type="email"
-						id="email"
-						value={emailState.value}
-						onChange={emailChangeHandler}
-						onBlur={validateEmailHandler}
-					/>
-				</div>
-				<div className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ""}`}>
-					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						id="password"
-						value={passwordState.value}
-						onChange={passwordChangeHandler}
-						onBlur={validatePasswordHandler}
-					/>
-				</div>
+				<Input
+					ref={emailInputRef}
+					id="email"
+					label="E-Mail"
+					type="email"
+					isValid={emailIsValid}
+					value={emailState.value}
+					onChange={emailChangeHandler}
+					onBlur={validateEmailHandler}
+				></Input>
+				<Input
+					ref={passwordInputRef}
+					id="password"
+					label="Password"
+					type="password"
+					isValid={passwordIsValid}
+					value={passwordState.value}
+					onChange={passwordChangeHandler}
+					onBlur={validatePasswordHandler}
+				></Input>
 				<div className={classes.actions}>
 					<Button
 						type="submit"
 						className={classes.btn}
-						disabled={!formIsValid}
 					>
 						Login
 					</Button>
--- Simple-React-Apps/Learning-useEffect-Hook/src/index.js
+++ Simple-React-Apps/Learning-useEffect-Hook/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { AuthContextProvider } from "./context/auth-context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// We moved logic from App to auth-context, to grap App inside it
+root.render(
+	<AuthContextProvider>
+		<App />
+	</AuthContextProvider>
+);
