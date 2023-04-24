306d995337b710c848d1bbc5651bc093a2c7959c
dosmiko7 <mikolaj.oberda@gmail.com>
2023-04-24 19:02:27 +0200

    Moved meals to database. Start creating form for order

--- Simple-React-Apps/FoodOrderApp/src/components/Meals/AvailableMeals.js
+++ Simple-React-Apps/FoodOrderApp/src/components/Meals/AvailableMeals.js
@@ -1,37 +1,65 @@
+import { useEffect, useState } from "react";
+
 import classes from "./AvailableMeals.module.css";
 
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-	{
-		id: "m1",
-		name: "Sushi",
-		description: "Finest fish and veggies",
-		price: 22.99,
-	},
-	{
-		id: "m2",
-		name: "Schnitzel",
-		description: "A german specialty!",
-		price: 16.5,
-	},
-	{
-		id: "m3",
-		name: "Barbecue Burger",
-		description: "American, raw, meaty",
-		price: 12.99,
-	},
-	{
-		id: "m4",
-		name: "Green Bowl",
-		description: "Healthy...and green...",
-		price: 18.99,
-	},
-];
-
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map((meal) => {
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState();
+
+	// We cant use useEffect(asyns () => {...})
+	// useEffect is supposed to be a function that returns either nothing (undefined) or a function (to clean up side effects). But an async function returns a Promise, which can't be called as a function!
+
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch("https://react-testfetch-default-rtdb.europe-west1.firebasedatabase.app/meals.json");
+			const responseData = await response.json();
+
+			if (!response.ok) {
+				throw new Error("Something went wrong!");
+			}
+
+			const loadedMeals = [];
+
+			for (const key in responseData) {
+				loadedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+
+			setMeals(loadedMeals);
+			setIsLoading(false);
+		};
+
+		fetchMeals().catch((error) => {
+			setIsLoading(false);
+			setHttpError(error.message);
+		});
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section className={classes.meals__loading}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	if (httpError) {
+		return (
+			<section className={classes.meals__error}>
+				<p>{httpError}</p>
+			</section>
+		);
+	}
+
+	const mealsList = meals.map((meal) => {
 		return (
 			<MealItem
 				key={meal.id}
