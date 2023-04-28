b68b92304b27155db431c4eada6839e71be09ce1
dosmiko7 <mikolaj.oberda@gmail.com>
2023-04-28 21:30:52 +0200

    useNavigation example

--- Simple-React-Apps/ReactRouter/frontend/src/pages/EventsPage.js
+++ Simple-React-Apps/ReactRouter/frontend/src/pages/EventsPage.js
@@ -7,7 +7,9 @@ function EventsPage() {
 	// 2.5 events will be the data returned by the loader in route in App.js
 	// In fact that we are using asyn in loader, the returned value will be a promise but React Router will automaticlly check that promise and return data for us
 	// 3. We could also get data in deeper components as like EventsList. But we can not do that in parent components (for example in App.js)
-	const events = useLoaderData();
+	// 6.5 We should make sure that we extract necessary data from response
+	const response = useLoaderData();
+	const events = response.events;
 
 	return <EventsList events={events} />;
 }
@@ -16,13 +18,14 @@ export default EventsPage;
 
 // 4. function for loader should be placed in the component where returned data will be used.
 // So, we are moving function from App.js here:
+// 5. Next step in Root.js
 
+// 6. We can return any data by loader. For example: Response which is a browser feature. Thanks to react router we can automatically get data from that response
 export const loader = async () => {
 	const response = await fetch("http://localhost:8080/events");
 	if (!response.ok) {
 		// ...
 	} else {
-		const resData = await response.json();
-		return resData.events;
+		return response;
 	}
 };
--- Simple-React-Apps/ReactRouter/frontend/src/pages/Root.js
+++ Simple-React-Apps/ReactRouter/frontend/src/pages/Root.js
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
+// 5. useNavigation lets us find out whether we're currently in active transition, if we're loading data or not.
+import { Outlet, useNavigation } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 
 const RootLayout = () => {
+	const navigation = useNavigation();
+	// 5.5 With that we can implement loading element which is shown only when we're in loading state
+	// 6. in EventsPage.js
 	return (
 		<>
 			<MainNavigation />
 			<main>
+				{navigation.state === "loading" ? <p>Loading...</p> : ""}
 				<Outlet />
 			</main>
 		</>
