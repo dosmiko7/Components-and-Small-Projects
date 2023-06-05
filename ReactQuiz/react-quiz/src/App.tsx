9a60b7921991a78bc1ea3e4424e59164ae871e04
dosmiko7 <mikolaj.oberda@gmail.com>
2023-06-05 16:49:05 +0200

    feat: added fetch data from json server

--- ReactQuiz/react-quiz/src/App.tsx
+++ ReactQuiz/react-quiz/src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+	useEffect(() => {
+		fetch(`http://localhost:8000/questions`)
+			.then((response) => response.json())
+			.then((data) => console.log(data))
+			.catch((err) => console.error("Error"));
+	});
+
 	return (
 		<div className="app">
 			<Header />
