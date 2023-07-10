891d21aedbb699bdc105a39abf9c957307c02a85
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-10 09:18:55 +0200

    Moved react-quiz outside

--- ReactQuiz/react-quiz/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const Footer = ({ children }: PropsWithChildren) => {
-	return <>{children}</>;
-};
-
-export default Footer;
--- ReactQuiz/react-quiz/src/components/Main.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const Main = ({ children }: PropsWithChildren) => {
-	return <main className="main">{children}</main>;
-};
-
-export default Main;
